228999ce5b7778e9aef60750c70c24aa30da6ebb
SurgeVortex <ac.chris.ackerman@gmail.com>
2014-04-23 15:58:52 +0200

    Completed initial File Class

--- file.cpp
+++ file.cpp
@@ -2,4 +2,70 @@
 
 File::File()
 {
+    this->fileName = new QFileInfo();
+}
+
+File::File (QFileInfo fileName)
+{
+    setFile (fileName);
+}
+
+File::File (QString absoluteFilePath)
+{
+    setFile (absoluteFilePath);
+}
+
+void File::setFile (QFileInfo fileName)
+{
+    this->fileName = new QFileInfo(fileName);
+    this->hashFile();
+}
+
+void File::setFile (QString absoluteFilePath)
+{
+    this->fileName = new QFileInfo(absoluteFilePath);
+    this->hashFile();
+}
+
+void File::hashFile()
+{
+    this->hashCode = QCryptographicHash::hash(getData(), QCryptographicHash::Sha1);
+}
+
+QString File::getAbsoluteFilePath ()
+{
+    return this->fileName->absoluteFilePath();
+}
+
+QString File::getAbsolutePath ()
+{
+    return this->fileName->absolutePath();
+}
+
+QString File::getFileName ()
+{
+    return this->fileName->fileName();
+}
+
+QString File::getFileSize ()
+{
+    return QString::number(this->fileName->size());
+}
+
+QString File::getDateCreated ()
+{
+    QDateTime temp = fileName->created();
+    QDateTime::
+    return "not yet implemented";
+}
+
+QByteArray File::getData()
+{
+    QFile file(fileName->absoluteFilePath());
+    return QByteArray(&file);
+}
+
+File::~File ()
+{
+
 }
--- file.h
+++ file.h
@@ -1,10 +1,36 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <QFileInfo>
+#include <QCryptographicHash>
+#include <QByteArray>
+#include <QFile>
+#include <QString>
+#include <QDir>
+#include <QDateTime>
+
 class File
 {
 public:
     File();
+    File(QFileInfo fileName);
+    File(QString absoluteFilePath);
+
+    ~File();
+
+    void setFile (QString absoluteFilePath);
+    void setFile (QFileInfo fileName);
+    void hashFile();
+
+    QString getFileSize();
+    QString getFileName();
+    QString getAbsoluteFilePath();
+    QString getAbsolutePath();
+    QString getDateCreated();
+    QByteArray getData();
+private:
+    QFileInfo * fileName;
+    QByteArray hashCode;
 };
 
 #endif // FILE_H
